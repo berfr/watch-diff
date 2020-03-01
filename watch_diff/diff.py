75602b38bf449775dc1c42f03b1dc62941d29a2b
berfr <me@berfr.me>
2020-02-29 23:02:34 -0500

    Run black formatter on all files

--- watch_diff/diff.py
+++ watch_diff/diff.py
@@ -7,11 +7,23 @@ from . import format
 
 
 class Diff(format.OutputFormatting):
-
     def __init__(self, a, b, previous_datetime, current_datetime):
         self._a = a
         self._b = b
-        self._diff = '\n'.join(difflib.unified_diff(a.splitlines(), b.splitlines(), 'Previous', 'Current', previous_datetime, current_datetime, lineterm='')) or None
+        self._diff = (
+            "\n".join(
+                difflib.unified_diff(
+                    a.splitlines(),
+                    b.splitlines(),
+                    "Previous",
+                    "Current",
+                    previous_datetime,
+                    current_datetime,
+                    lineterm="",
+                )
+            )
+            or None
+        )
 
     def __bool__(self):
         return bool(self._diff)
@@ -20,12 +32,22 @@ class Diff(format.OutputFormatting):
         lines = self._diff.splitlines()
         output = []
         for line in lines[:2]:
-            output.append('{}{}{}'.format(formatter.header_start, line, formatter.header_end))
+            output.append(
+                "{}{}{}".format(formatter.header_start, line, formatter.header_end)
+            )
         for line in lines[2:]:
-            if line[0] == '+':
-                output.append('{}{}{}'.format(formatter.addition_start, line, formatter.addition_end))
-            elif line[0] == '-':
-                output.append('{}{}{}'.format(formatter.subtraction_start, line, formatter.subtraction_end))
+            if line[0] == "+":
+                output.append(
+                    "{}{}{}".format(
+                        formatter.addition_start, line, formatter.addition_end
+                    )
+                )
+            elif line[0] == "-":
+                output.append(
+                    "{}{}{}".format(
+                        formatter.subtraction_start, line, formatter.subtraction_end
+                    )
+                )
             else:
                 output.append(line)
-        return '\n'.join(output)
+        return "\n".join(output)
